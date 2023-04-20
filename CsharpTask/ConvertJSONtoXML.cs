a851b9d435c31772a6da2d6f4bad2b023e8d8335
DolevMishali <dolevmi2010@gmail.com>
2023-04-20 21:25:55 +0300

    finish converter

--- CsharpTask/ConvertJSONtoXML.cs
+++ CsharpTask/ConvertJSONtoXML.cs
@@ -1,54 +1,60 @@
-﻿using Newtonsoft.Json;
-using JsonSerializer = System.Text.Json.JsonSerializer;
-using Newtonsoft.Json.Linq;
-using System.Xml.Linq;
+﻿using Newtonsoft.Json.Linq; // JObject using Linq
+using System.Xml.Linq; // XElement using Linq
 
-namespace ReadJSONFile
+namespace CSharpTask
 {
-    class Convert
+    class Json2XMLConverter
     {
         static void Main()
         {
             string currentDir = Directory.GetCurrentDirectory();
             string inputFilePath = Path.Combine(currentDir, @"..\..\..\..\exam.json");
             string outputFilePath = Path.Combine(currentDir, @"..\..\..\..\exam.xml");
-            StreamReader sr = new StreamReader(inputFilePath);
-            string jsonString = sr.ReadToEnd();
-            Dictionary<string, object> wrapperDict = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString);
+            string jsonString = File.ReadAllText(inputFilePath);
+            string xmlOutput = "";
+            JObject json = JObject.Parse(jsonString);
 
-            foreach (KeyValuePair<string, object> keyValuePair in wrapperDict)
+            if(true)
             {
-                if (keyValuePair.Value != null)
+                // Create a new XElement for the root element of the XML
+                XElement root = new XElement("Root");
+
+                // Loop through each person in the JSON object
+                foreach (JObject person in json["persons"])
                 {
-                    var arrayWrapper = keyValuePair.Value.ToString().Split('{');
-                    Dictionary<string, int> innerDict = arrayWrapper.ToDictionary(x => x, x => x.Length);
-                    for (int i = 0; i < innerDict.Count; i++)
-                    {
-                        Console.WriteLine();
-                    }
+                    // Create a new XElement for the person
+                    XElement personElement = new XElement("persons");
+
+                    // Add the person's elements with XElement name/person, age, occupation
+                    if (person["name"] is null)
+                        personElement.Add(new XElement("person", (string)person["person"]));
+                    else
+                        personElement.Add(new XElement("name", (string)person["name"]));
+
+                    personElement.Add(new XElement("age", (int)person["age"]));
+                    personElement.Add(new XElement("occupation", (string)person["occupation"]));
+
+                    // Add the person XElement to the root XElement
+                    root.Add(personElement);
                 }
+
+                xmlOutput = root.ToString();
+            }
+            else
+            {
+                /* ----------- The easiest way using newton external package ----------- */
+
+                // Convert JSON to XML
+                JObject jsonObject = JObject.Parse(jsonString);
+                XNode convertedXml = Newtonsoft.Json.JsonConvert.DeserializeXNode(jsonObject.ToString(), "Root");
+                xmlOutput = convertedXml.ToString();
             }
 
-
-
-            /* ----------- The easies way using newton external package ----------- */
-
-            //// Read JSON input file
-            //string jsonString = File.ReadAllText(inputFilePath);
-
-            //// Convert JSON to XML
-            //JObject jsonObject = JObject.Parse(jsonString);
-            //XNode convertedXml = JsonConvert.DeserializeXNode(jsonObject.ToString(), "Root");
-
-            //// Save XML output to file
-            //using (var writer = new StreamWriter(outputFilePath))
-            //{
-            //    writer.Write(convertedXml.ToString());
-            //}
+            // Save XML output to file
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                writer.Write(xmlOutput);
+            }
         }
     }
 }
-
-
-
-
